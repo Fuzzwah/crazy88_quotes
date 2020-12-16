de6812983d47f180200572bf5237d8107c110022
Fuzzwah <rob.crouch@gmail.com>
2020-12-17 07:15:59 +1100

    random

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,13 +5,12 @@ from . import views
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'quotes', views.QuotesViewSet)
-router.register(r'random', views.RandomQuoteView.as_view(), basename='random')
-
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^random$', views.RandomQuoteView.as_view(), name='random'),
     url(r'^quote/(?P<id>[0-9]+)/$', views.QuoteView.as_view(), name='quote'),
 ]
 
