4b817e19bb22a3fc7334e415d0c705be00e69dbe
Fuzzwah <rob.crouch@gmail.com>
2020-12-17 09:01:13 +1100

    pare down

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,16 +3,10 @@ from django.urls import path
 from rest_framework import routers
 from . import views
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'all', views.QuotesViewSet)
-
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
     path('random', views.random_quote),
-    url(r'quote/(?P<id>[0-9]+)/$', views.QuoteView.as_view(), name='quote'),
 ]
 
 app_name = 'quotes'
--- quotes/views.py
+++ quotes/views.py
@@ -1,42 +1,9 @@
 from random import choice
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.utils.translation import ugettext_lazy as _
-from django.db import transaction
-from django.shortcuts import render
-from django.conf import settings
-from django_tables2 import SingleTableView
-from django.shortcuts import render, HttpResponseRedirect, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.db.models import Max
-from rest_framework.renderers import JSONRenderer
-from rest_framework import viewsets, generics
+from django.http import JsonResponse
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.decorators import api_view
-
-class SlackSingleQuoteRenderer(JSONRenderer):
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        data = {
-            "response_type": "in_channel",
-            "text": data,
-            #"attachments": [{"text": data[0]['text']}]
-        }
-        return super(SlackSingleQuoteRenderer, self).render(data, accepted_media_type, renderer_context)
-
-from quotes.serializers import (
-    QuoteSerializer,
-)
-
-from quotes.models import (
-    Quote,
-)
-
-class QuotesViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Quote.objects.all()
-    serializer_class = QuoteSerializer
-
-
+from quotes.serializers import QuoteSerializer
+from quotes.models import Quote
 
 @api_view(['POST'])
 @authentication_classes([])
@@ -54,13 +21,3 @@ def random_quote(request):
     }
     return JsonResponse(data)
 
-@api_view(['POST'])
-@authentication_classes([])
-@permission_classes([])
-class QuoteView(generics.ListAPIView):
-    renderer_classes = (SlackSingleQuoteRenderer, )
-    queryset = Quote.objects.all()
-    serializer_class = QuoteSerializer
-
-    def get_queryset(self, *args, **kwargs):
-        return self.queryset.filter(id=self.kwargs.get('id'))
