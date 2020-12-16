7b719ccb0a3f1455a5676e2fa103c7ddda7dedb0
Fuzzwah <rob.crouch@gmail.com>
2020-12-17 10:15:31 +1100

    pare down

--- quotes/views.py
+++ quotes/views.py
@@ -25,19 +25,33 @@ def random_quote(request):
 @authentication_classes([])
 @permission_classes([])
 def get_quote(request):
+    quote_id = False
     payload_list = str(request.body).split('&')
     for item in payload_list:
         key, val = item.split('=')
         if key == 'text':
             quote_id = val
             break
+    if not quote_id:
+        data = {
+            "response_type": "in_channel",
+            "text": f"You need to provide a quote number!",
+        }
+        return JsonResponse(data)
+
 
     quote = Quote.objects.all().filter(id=quote_id)
     serializer = QuoteSerializer(quote, many=True)
-    data = {
-        "response_type": "in_channel",
-        "text": f"Quote #{serializer.data[0]['id']}",
-        "attachments": [{"text": serializer.data[0]['text']}]
-    }
+    try:
+        data = {
+            "response_type": "in_channel",
+            "text": f"Quote #{serializer.data[0]['id']}",
+            "attachments": [{"text": serializer.data[0]['text']}]
+        }
+    except IndexError:
+        data = {
+            "response_type": "in_channel",
+            "text": f"Quote {quote_id} not found in the database",
+        }
     return JsonResponse(data)
 
