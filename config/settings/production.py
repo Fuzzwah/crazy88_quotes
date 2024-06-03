53fe455bfb4f9d5ff6b20792bd3f30e88bbefdd4
Fuzzwah <rob.crouch@gmail.com>
2024-06-03 12:03:52 +1000

    yolo

--- config/settings/production.py
+++ config/settings/production.py
@@ -59,7 +59,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
     },
 }
 
